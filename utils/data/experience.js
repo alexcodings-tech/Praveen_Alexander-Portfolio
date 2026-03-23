8911668947e4e2b0b5be2a2d721556946cd52112
Praveen Alexander <praveenalexander5@gmail.com>
2026-03-23 19:33:43 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,14 +1,8 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Free lancing",
-    duration: "(Jan 2024 - Present)"
-  },
-  {
-    id: 2,
-    title: 'Software Developer - Team Lead',
-    company: "The ASK Company ltd.",
-    duration: "(Jan 2025 - Present)"
+    title: 'Full Stack Developer',
+    company: "Zhar Tech",
+    duration: "(Jan 2026 - Present)"
   }
 ]
