d81b7bf20f0055e091e679b3586370b46fb3b20e
Praveen Alexander <praveenalexander5@gmail.com>
2026-03-25 18:26:52 +0530

    Update index.jsx

--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -18,7 +18,7 @@ export default function Experience() {
   return (
     <div
       id="experience"
-      className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]"
+      className="relative z-50 border-t my-6 lg:my-12 border-[#25213b]"
     >
       {/* Section Background */}
       <Image
@@ -41,24 +41,24 @@ export default function Experience() {
       </div>
 
       {/* Section Content */}
-      <div className="py-8">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
+      <div className="py-4">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8">
           {/* Animation Section */}
           <div className="flex justify-center items-start">
-            <div className="w-full h-full">
+            <div className="w-[70%] md:w-[60%] mx-auto">
               <AnimationLottie animationPath={experience} />
             </div>
           </div>
 
           {/* Experience Cards */}
           <div>
-            <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-3">
               {experiences.map((exp) => (
                 <GlowCard
                   key={exp.id}
                   identifier={`experience-${exp.id}`}
                 >
-                  <div className="p-3 relative text-white">
+                  <div className="p-2 relative text-white">
                     <Image
                       src="/blur-23.svg"
                       alt="Blur Background"
@@ -73,10 +73,10 @@ export default function Experience() {
                     </div>
                     <div className="flex items-center gap-x-8 px-3 py-5">
                       <div className="text-violet-500 transition-all duration-300 hover:scale-125">
-                        <BsPersonWorkspace size={36} />
+                        <BsPersonWorkspace size={26} />
                       </div>
                       <div>
-                        <p className="text-base sm:text-xl mb-2 font-medium uppercase">
+                        <p className="text-sm sm:text-lg mb-2 font-medium uppercase">
                           {exp.title}
                         </p>
                         <p className="text-sm sm:text-base">{exp.company}</p>
